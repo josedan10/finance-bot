34c1400eb8d891fd1b14a338e6628f2ccd439f04
José Daniel <josedanq100@gmail.com>
2023-09-16 23:14:25 -0400

    fix: test puppeteer on server

---
+++ modules/logger/logger.module.js
@@ -0,0 +1,9 @@
+import winston from 'winston';
+
+export const Logger = winston.createLogger({
+	transports: [
+		new winston.transports.Console(),
+		new winston.transports.File({ filename: 'error.log', level: 'error' }),
+		new winston.transports.File({ filename: 'combined.log' }),
+	],
+});
